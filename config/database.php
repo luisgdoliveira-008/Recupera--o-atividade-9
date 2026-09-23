59f8d8f8a8e2bf1a675c320756b910f01a63face
luisgdoliveira-008 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-09-23 08:58:35 -0300

    adicionei conexão com o banco

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$dbname = "mercado_estoque";
+$user = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    die("Erro ao conectar ao banco de dados.");
+}
